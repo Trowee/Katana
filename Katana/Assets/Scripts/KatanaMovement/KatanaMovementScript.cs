2635aacba30efd110d09230fdad6f0b27ab30b2c
nnra6864 <enenragd@gmail.com>
2025-01-18 06:50:59 +0100

    Started implementing the strike ability

--- Katana/Assets/Scripts/KatanaMovement/KatanaMovementScript.cs
+++ Katana/Assets/Scripts/KatanaMovement/KatanaMovementScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Core;
 using NnUtils.Scripts;
 using TimeScale;
@@ -12,29 +13,29 @@ namespace KatanaMovement
         [Header("Components")]
         [SerializeField] private Rigidbody _rb;
         [SerializeField] private Transform _model;
+
+        [Header("Tilt")]
+        [SerializeField] private float _tiltSpeed = 90;
        
         [Header("Jump")]
-        [SerializeField] private Vector2 _jumpForce;
-        [SerializeField] private float _jumpRotation;
+        [SerializeField] private Vector2 _jumpForce = new(10, 15);
+        [SerializeField] private float _jumpRotation = 10;
         [SerializeField] private TimeScaleKeys _jumpTimeScale;
         
         [Header("Dash")]
-        [SerializeField] private float _dashForce;
+        [SerializeField] private float _dashForce = 30;
         [SerializeField] private TimeScaleKeys _dashTimeScale;
 
-        [Header("Tilt")]
-        [SerializeField] private float _tiltAmount;
-        [SerializeField] private float _maxTilt;
-        private float _defaultTilt, _currentTilt;
+        [Header("Strike")]
+        [SerializeField] private float _strikeTransitionTime = 5;
+        [SerializeField] private float _strikeHeight = 100;
+        [SerializeField] private float _strikeForce = 100;
+        [SerializeField] private TimeScaleKeys _strikeHoverTimeScale;
+        [SerializeField] private TimeScaleKeys _strikeImpactTimeScale;
 
         private void Reset()
         {
             _rb = GetComponent<Rigidbody>();
-        }
-
-        private void Awake()
-        {
-            _defaultTilt = _model.localRotation.eulerAngles.z;
         }
 
         private void Update()
@@ -42,6 +43,7 @@ namespace KatanaMovement
             Tilt();
             if (Input.GetKeyDown(KeyCode.Space)) Jump();
             if (Input.GetKeyDown(KeyCode.Mouse0)) Dash();
+            if (Input.GetKeyDown(KeyCode.Mouse1)) Strike();
         }
 
         private Vector3 GetForward()
@@ -51,8 +53,24 @@ namespace KatanaMovement
             return noX * Vector3.forward;
         }
 
+        private void Tilt()
+        {
+            var amount = Input.GetAxisRaw("Horizontal") * Time.unscaledDeltaTime * _tiltSpeed;
+
+            // Get the current rotation
+            var currentRotation = transform.localRotation;
+
+            // Define the tilt as a rotation around the Y axis
+            var tilt = Quaternion.AngleAxis(amount, Vector3.up);
+
+            // Combine the rotations
+            transform.localRotation = currentRotation * tilt;
+        }
+
         private void Jump()
         {
+            if (_strikeRoutine != null) return;
+            
             // Store forward
             var forward = GetForward();
             
@@ -70,6 +88,8 @@ namespace KatanaMovement
 
         private void Dash()
         {
+            if (_strikeRoutine != null) return;
+            
             // Reset velocity
             _rb.linearVelocity = Vector3.zero;
             _rb.angularVelocity = Vector3.zero;
@@ -81,19 +101,28 @@ namespace KatanaMovement
             GameManager.TimeScaleManager.UpdateTimeScale(_dashTimeScale);
         }
 
-        private void Tilt()
+        private void Strike()
         {
-            float amount = Input.GetAxisRaw("Horizontal") * Time.unscaledDeltaTime * _tiltAmount;
-
-            // Get the current rotation
-            Quaternion currentRotation = transform.localRotation;
-
-            // Define the tilt as a rotation around the Z axis
-            Quaternion tilt = Quaternion.AngleAxis(amount, Vector3.up);
-
-            // Combine the rotations
-            transform.localRotation = currentRotation * tilt;
+            if (_strikeRoutine != null) return;
+            this.RestartRoutine(ref _strikeRoutine, StrikeRoutine());
         }
-        
+
+        private Coroutine _strikeRoutine;
+        private IEnumerator StrikeRoutine()
+        {
+            // Disable gravity
+            _rb.useGravity = false;
+            
+            // Reset velocity
+            _rb.linearVelocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+            
+            // Adjust transform
+            transform.localPosition += Vector3.up * 100;
+            transform.localRotation = Quaternion.Euler(0, transform.localRotation.eulerAngles.y, 0);
+            
+            yield return new WaitForSeconds(5);
+            _strikeRoutine = null;
+        }
     }
 }
