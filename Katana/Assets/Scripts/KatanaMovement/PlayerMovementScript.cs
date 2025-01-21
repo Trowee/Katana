8e9e114387707deb4d96017c1e8c9c0a9f2e34b5
nnra6864 <enenragd@gmail.com>
2025-01-21 05:23:41 +0100

    Implemented sticking

--- Katana/Assets/Scripts/KatanaMovement/PlayerMovementScript.cs
+++ Katana/Assets/Scripts/KatanaMovement/PlayerMovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Core;
 using NnUtils.Scripts;
@@ -12,11 +13,14 @@ namespace KatanaMovement
     {
         private static Camera Cam => PSM.CameraManager.Camera;
         private static string CamHandler => PSM.PlayerCameraHandler;
+
+        /// Whether the player is currently stuck
+        [ReadOnly] public bool IsStuck;
         
         /// Whether the player is currently using the strike ability
         [ReadOnly] public bool IsPerformingStrike;
         
-        // Whether the player is performing the strike impact
+        /// Whether the player is performing the strike impact
         [ReadOnly] public bool IsPerformingStrikeImpact;
         
         [Header("Components")]
@@ -29,6 +33,10 @@ namespace KatanaMovement
         
         [Header("Tilt")]
         [SerializeField] private float _tiltSpeed = 1;
+        
+        [Header("Stick")]
+        [SerializeField] private float _minStickVelocity = 5;
+        [SerializeField] private float _maxStickAngle = 45;
        
         [Header("Flip")]
         [SerializeField] private Vector2 _flipForce = new(20, 25);
@@ -75,18 +83,91 @@ namespace KatanaMovement
             if (Input.GetKeyDown(KeyCode.S)) Strike();
         }
 
-        private void OnCollisionEnter(Collision collision)
+        private void OnCollisionEnter(Collision col)
         {
+            // If performing strike, check if something's hit
             if (IsPerformingStrikeImpact)
             {
-                if (collision.gameObject.layer == LayerMask.NameToLayer("Default"))
-                { StrikeImpact(); return; }
+                // If there is no hit, do an early return
+                if ((_strikeLayerMask & (1 << col.gameObject.layer)) == 0) return;
+                
+                // Handle the strike impact
+                StrikeImpact(col);
+                
+                // Return
+                return;
             }
             
+            // Try to stick
+            Stick(col);
+        }
+
+        private void Stick(Collision col)
+        {
+            // Return if already stuck
+            if (IsStuck) return;
+            
+            // Return if striking
+            if (IsPerformingStrike || IsPerformingStrikeImpact) return;
+            
+            // Return if not moving forward fast enough
+            if (col.relativeVelocity.magnitude < _minStickVelocity) return;
+            //if (transform.InverseTransformDirection(_rb.linearVelocity).y < _minStickVelocity) return;
+
+            // Get forward and threshold
+            var fw = transform.up;
+            var threshold = -Mathf.Cos(_maxStickAngle * Mathf.Deg2Rad);
+
+            // Loop through all normals and see if they are at a low enough angle relative to player
+            foreach (var contact in col.contacts)
+            {
+                // Get the normal and dot product
+                var contactNormal = contact.normal;
+                var dp = Vector3.Dot(fw, contactNormal);
+                
+                // Return if angle is not low enough
+                if (dp > threshold) continue;
+                
+                // Get stuck and return
+                GetStuck(col.transform);
+                return;
+            }
+        }
+
+        private void GetStuck(Transform parent)
+        {
+            // Freeze rb
+            _rb.linearVelocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+            _rb.isKinematic = true;
+            
+            // Set parent to follow that object
+            transform.SetParent(parent);
+            
+            // Set IsStuck to true
+            IsStuck = true;
+        }
+
+        private void GetUnstuck()
+        {
+            // Return if not stuck
+            if (!IsStuck) return;
+            
+            // Unfreeze the rb
+            _rb.isKinematic = false;
+            
+            // Reset parent
+            transform.SetParent(null);
+            
+            // Set IsStuck to false
+            IsStuck = false;
         }
 
         private void Tilt()
         {
+            // Return if stuck
+            if (IsStuck) return;
+            
             // Store mouse input
             var x = Input.GetAxisRaw("Mouse X") * _tiltSpeed;
             var y = Input.GetAxisRaw("Mouse Y") * _tiltSpeed;
@@ -116,6 +197,8 @@ namespace KatanaMovement
 
         private void Flip()
         {
+            GetUnstuck();
+            
             // Store forward
             var forward = transform.up;
             
@@ -133,6 +216,8 @@ namespace KatanaMovement
 
         private void Dash()
         {
+            GetUnstuck();
+            
             // Reset velocity
             _rb.linearVelocity = Vector3.zero;
             _rb.angularVelocity = Vector3.zero;
@@ -146,6 +231,8 @@ namespace KatanaMovement
 
         private void Dodge()
         {
+            GetUnstuck();
+            
             // Reset velocity
             _rb.linearVelocity = Vector3.zero;
             _rb.angularVelocity = Vector3.zero;
@@ -159,7 +246,7 @@ namespace KatanaMovement
 
         private void Strike()
         {
-            if (_strikeRoutine != null) return;
+            if (IsPerformingStrike || IsPerformingStrikeImpact) return;
             this.RestartRoutine(ref _strikeRoutine, StrikeRoutine());
         }
 
@@ -169,15 +256,16 @@ namespace KatanaMovement
         {
             IsPerformingStrike = true;
             
-            // Disable gravity
-            _rb.useGravity = false;
+            // Unstick
+            GetUnstuck();
+            
+            // Freeze rb
+            _rb.linearVelocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+            _rb.isKinematic = true;
             
             // Disable collider
             _collider.enabled = false;
-            
-            // Reset velocity
-            _rb.linearVelocity = Vector3.zero;
-            _rb.angularVelocity = Vector3.zero;
             
             // Store transform
             var startPos = transform.localPosition;
@@ -235,7 +323,7 @@ namespace KatanaMovement
             // Update values
             IsPerformingStrike       = false;
             IsPerformingStrikeImpact = true;
-            _rb.useGravity           = true;
+            _rb.isKinematic          = false;
             _collider.enabled        = true;
 
             // Add strike to where the blade is pointing
@@ -243,9 +331,10 @@ namespace KatanaMovement
             GameManager.TimeScaleManager.UpdateTimeScale(_strikeImpactTimeScale);
         }
 
-        private void StrikeImpact()
+        private void StrikeImpact(Collision col)
         {
             IsPerformingStrikeImpact = false;
+            Stick(col);
             GameManager.TimeScaleManager.UpdateTimeScale(_postStrikeImpactTimeScale);
         }
     }
