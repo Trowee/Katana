0e4ce8bebcb199467ce0da2131775f6b9acc01f4
nnra6864 <enenragd@gmail.com>
2025-01-27 09:14:01 +0100

    Improved DOF to work with all perspectives and started reimplementing the main menu

--- Katana/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Katana/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using NnUtils.Scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +10,12 @@ namespace Assets.Scripts.MainMenu
         private static MainMenuManager _instance;
         public static MainMenuManager Instance => _instance = FindFirstObjectByType<MainMenuManager>();
 
+        [SerializeField] private Transform _cameraHolder;
+        [SerializeField] private float _menuTransitionTime;
+        [SerializeField] private AnimationCurve _menuTransitionCurve;
+        [SerializeField] private Vector3 _mainMenuRotation;
+        [SerializeField] private Vector3 _settingsRotation = new(0, 180, 0);
+        
         private void Awake()
         {
             if (_instance != null && _instance != this)
@@ -20,13 +27,40 @@ namespace Assets.Scripts.MainMenu
             _instance = this;
         }
 
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) MainMenu();
+            if (Input.GetKeyDown(KeyCode.Alpha1)) MainMenu();
+            if (Input.GetKeyDown(KeyCode.Alpha2)) Settings();
+        }
+
         public void Play() => SceneManager.LoadScene(1);
 
+        public void MainMenu()
+        {
+            this.RestartRoutine(ref _rotateCameraRoutine, RotateCameraRoutine(_mainMenuRotation));
+        }
+        
         public void Settings()
         {
-            
+            this.RestartRoutine(ref _rotateCameraRoutine, RotateCameraRoutine(_settingsRotation));
         }
         
         public void Quit() => Application.Quit();
+
+        private Coroutine _rotateCameraRoutine;
+
+        private IEnumerator RotateCameraRoutine(Vector3 targetRot)
+        {
+            var startRot = _cameraHolder.localEulerAngles;
+
+            float lerpPos = 0;
+            while (lerpPos < 1)
+            {
+                var t = _menuTransitionCurve.Evaluate(Misc.Tween(ref lerpPos, _menuTransitionTime, unscaled: true));
+                _cameraHolder.localRotation = Quaternion.Euler(Vector3.LerpUnclamped(startRot, targetRot, t));
+                yield return null;
+            }
+        }
     }
 }
