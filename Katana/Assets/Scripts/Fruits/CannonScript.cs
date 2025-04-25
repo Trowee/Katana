370d96cb0345fbd06670c4bff0f32592fa5f23f8
nnra6864 <enenragd@gmail.com>
2025-04-25 13:33:42 +0200

    Implemented the new cannon animation for the most part, I might add one more keyframe but idk

--- Katana/Assets/Scripts/Fruits/CannonScript.cs
+++ Katana/Assets/Scripts/Fruits/CannonScript.cs
@@ -7,17 +7,34 @@ namespace Assets.Scripts.Fruits
 {
     public class CannonScript : MonoBehaviour
     {
+        [SerializeField] private Transform _cannon;
         [SerializeField] private Transform _barrel;
-        [SerializeField] private Transform _spawnPoint;
-        [SerializeField] private List<Rigidbody> _fruits;
-
-        [SerializeField] private Vector2 _shootingRotRange = new(-20, 10);
+        [SerializeField] private Transform _wheels;
+        [SerializeField] private Transform _projectileSpawnPoint;
+        
+        [Header("Shooting")]
+        
         [SerializeField] private Vector2 _cooldownRange = new(10, 20);
-        [SerializeField] private Vector2 _shootingForceRange = new(25, 75);
-        [SerializeField] private Vector2 _torqueRange = new(-50, 50);
+        [SerializeField] private Vector2 _aimCooldownRange = new(0.5f, 1.5f);
+        [SerializeField] private Vector2 _shootingForceRange = new(20, 50);
+        [SerializeField] private Vector2 _shootingTorqueRange = new(-50, 50);
+        [SerializeField] private List<Rigidbody> _projectiles;
 
-        [SerializeField] private float _aimTime = 1;
+        [Header("Aim Animation", order = 1)]
+        
+        [SerializeField] private Vector2 _aimAnimTimeRange = new(0.75f, 1.25f);
         [SerializeField] private AnimationCurve _aimCurve;
+        [SerializeField] private Vector4 _aimPosRange = new(-1.5f, 1.5f, 1, 3);
+        [SerializeField, Range(0, 1)] private float _aimRotIntensity = 0.25f;
+        [SerializeField] private Vector2 _aimBarrelRotationRange = new(-20, 0);
+        [SerializeField] private float _wheelRotIntensity = 40;
+        
+        [Header("Shoot Animation", order = 1)]
+        
+        [SerializeField] private Vector2 _shootAnimTimeRange = new(0.75f, 1);
+        [SerializeField] private AnimationCurve _shootCurve;
+
+        private Quaternion _baseBarrelRot;
 
         private IEnumerator Start()
         {
@@ -25,36 +42,77 @@ namespace Assets.Scripts.Fruits
             {
                 yield return new WaitForSeconds(Random.Range(_cooldownRange.x, _cooldownRange.y));
 
-                var startRot = _barrel.localEulerAngles;
-                var targetRot = startRot;
-                targetRot.x = Random.Range(_shootingRotRange.x, _shootingRotRange.y);
+                // Store Cannon Data
+                var startCannonPos = _cannon.localPosition;
+                var targetCannonPos = startCannonPos;
+                var startCannonRot = _cannon.localRotation;
+
+                // Calculate Target Cannon Pos, Dir and Distance Data
+                targetCannonPos.x += Random.Range(_aimPosRange.x, _aimPosRange.y);
+                targetCannonPos.z += Random.Range(_aimPosRange.z, _aimPosRange.w);
+                var cannonMoveDir = (targetCannonPos - startCannonPos).normalized;
+                var moveDistance = Vector3.Distance(targetCannonPos, startCannonPos);
+
+                // Calculate Cannon Rot Data
+                var targetAngle = Mathf.Atan2(cannonMoveDir.x, cannonMoveDir.z) * Mathf.Rad2Deg;
+                var rotationAmount = targetAngle * _aimRotIntensity * moveDistance;
+                var targetCannonRot =
+                    Quaternion.Euler(0, startCannonRot.eulerAngles.y + rotationAmount, 0);
                 
+                // Store Barrel Rot Data
+                var startBarrelRot = _barrel.localEulerAngles;
+                var targetBarrelRot = startBarrelRot;
+                targetBarrelRot.x =
+                    Random.Range(_aimBarrelRotationRange.x, _aimBarrelRotationRange.y);
+                
+                // Calculate Wheels Rotation
+                var startWheelRot = _wheels.localRotation;
+                var targetWheelRot = Quaternion.Euler(
+                    startWheelRot * (Vector3.right * moveDistance * _wheelRotIntensity));
+                
+                var aimTime = Random.Range(_aimAnimTimeRange.x, _aimAnimTimeRange.y);
                 float lerpPos = 0;
                 while (lerpPos < 1)
                 {
-                    var t = _aimCurve.Evaluate(Misc.Tween(ref lerpPos, _aimTime));
-                    _barrel.localRotation = Quaternion.Euler(Vector3.LerpUnclamped(startRot, targetRot, t));
+                    var t = _aimCurve.Evaluate(Misc.Tween(ref lerpPos, aimTime));
+                    _cannon.localPosition =
+                        Vector3.SlerpUnclamped(startCannonPos, targetCannonPos, t);
+                    _cannon.localRotation =
+                        Quaternion.SlerpUnclamped(startCannonRot, targetCannonRot, t);
+                    _barrel.localRotation = Quaternion.Euler(
+                        Vector3.LerpUnclamped(startBarrelRot, targetBarrelRot, t));
+                    _wheels.localRotation =
+                        Quaternion.SlerpUnclamped(startWheelRot, targetWheelRot, t);
                     yield return null;
                 }
 
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(
+                    Random.Range(_aimCooldownRange.x, _aimCooldownRange.y));
                 
-                var f = _fruits[Random.Range(0, _fruits.Count)];
-                var fruit = Instantiate(f, _spawnPoint.position, _spawnPoint.rotation);
+                var f = _projectiles[Random.Range(0, _projectiles.Count)];
+                var fruit = Instantiate(f, _projectileSpawnPoint.position, _projectileSpawnPoint.rotation);
 
                 var shootingForce = Random.Range(_shootingForceRange.x, _shootingForceRange.y);
                 fruit.AddRelativeForce(Vector3.forward * shootingForce, ForceMode.Impulse);
                 
-                var shootingTorque = Random.Range(_torqueRange.x, _torqueRange.y);
+                var shootingTorque = Random.Range(_shootingTorqueRange.x, _shootingTorqueRange.y);
                 fruit.AddTorque(
                     new Vector3(Misc.Random1, Misc.Random1, Misc.Random1) * shootingTorque,
                     ForceMode.Impulse);
-                
+
+                var shootTime = Random.Range(_shootAnimTimeRange.x, _shootAnimTimeRange.y);
                 lerpPos = 0;
                 while (lerpPos < 1)
                 {
-                    var t = _aimCurve.Evaluate(Misc.Tween(ref lerpPos, _aimTime));
-                    _barrel.localRotation = Quaternion.Euler(Vector3.LerpUnclamped(targetRot, startRot, t));
+                    var t = _shootCurve.Evaluate(Misc.Tween(ref lerpPos, shootTime));
+                    _cannon.localPosition =
+                        Vector3.SlerpUnclamped(startCannonPos, targetCannonPos, t);
+                    _cannon.localRotation =
+                        Quaternion.SlerpUnclamped(startCannonRot, targetCannonRot, t);
+                    _barrel.localRotation = Quaternion.Euler(
+                        Vector3.LerpUnclamped(startBarrelRot, targetBarrelRot, t));
+                    _wheels.localRotation =
+                        Quaternion.SlerpUnclamped(startWheelRot, targetWheelRot, t);
                     yield return null;
                 }
             }
