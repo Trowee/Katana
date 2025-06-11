11e071bfce426b698db1f9d8ea53e9a062d96880
nnra6864 <enenragd@gmail.com>
2025-06-12 00:36:26 +0200

    Improved the displayed item drastically

---
+++ Katana/Assets/Scripts/MainMenu/Shop/SpotlightRotatorScript.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [ExecuteAlways]
+    public class SpotlightRotatorScript : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotationSpeed = Vector3.forward * 20;
+        
+        private void Update() => transform.Rotate(_rotationSpeed * Time.deltaTime);
+    }
+}
