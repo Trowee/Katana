077263e413b90d57c2d0b8d76a8ede37cc91490d
nnra6864 <enenragd@gmail.com>
2025-06-16 23:16:35 +0200

    Added a simple build script

---
+++ Katana/Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,67 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public static class BuildScript
+{
+    private static readonly string[] scenes = FindEnabledEditorScenes();
+    private const string GameName = "Escapeon";
+
+    [MenuItem("File/Build")]
+    public static void Build()
+    {
+        var buildsDir = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "..", "Builds"));
+
+        //BuildForWindows(buildsDir);
+        BuildForLinux(buildsDir);
+    }
+
+    public static void BuildForWindows(string buildsDir)
+    {
+        string path = Path.Combine(buildsDir, "Windows");
+        EnsureDirectoryExists(path);
+
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            target = BuildTarget.StandaloneWindows64,
+            locationPathName = Path.Combine(path, $"{GameName}.exe"),
+            options = BuildOptions.None
+        };
+
+        var result = BuildPipeline.BuildPlayer(options);
+        Debug.Log("Windows build result: " + result.summary.result);
+    }
+
+    public static void BuildForLinux(string buildsDir)
+    {
+        string path = Path.Combine(buildsDir, "Linux");
+        EnsureDirectoryExists(path);
+
+        BuildPlayerOptions options = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            target = BuildTarget.StandaloneLinux64,
+            locationPathName = Path.Combine(path, $"{GameName}.x86_64"),
+            options = BuildOptions.None
+        };
+
+        var result = BuildPipeline.BuildPlayer(options);
+        Debug.Log("Linux build result: " + result.summary.result);
+    }
+
+    private static string[] FindEnabledEditorScenes() =>
+        EditorBuildSettings.scenes
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
+            .ToArray();
+
+    private static void EnsureDirectoryExists(string path)
+    {
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+    }
+}
+#endif
