4073a67d413802ed191e7758fb8d6fe593c0ef1a
nnra6864 <enenragd@gmail.com>
2025-06-19 22:52:57 +0200

    Wrapped the thing in Unity editor, hopefully it builds

--- Katana/Assets/Scripts/LocalizeDropdown.cs
+++ Katana/Assets/Scripts/LocalizeDropdown.cs
@@ -1,42 +1,49 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
-using TMPro;
-using UnityEditor;
 
-public class LocalizeDropdown : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] private List<LocalizedString> _dropdownOptions;
-    private TMP_Dropdown _tmpDropdown;
 
-    private void Awake()
+    public class LocalizeDropdown : MonoBehaviour
     {
-        if (_tmpDropdown == null)
-            _tmpDropdown = GetComponent<TMP_Dropdown>();
+        [SerializeField] private List<LocalizedString> _dropdownOptions;
+        private TMP_Dropdown _tmpDropdown;
+
+        private void Awake()
+        {
+            if (!_tmpDropdown) _tmpDropdown = GetComponent<TMP_Dropdown>();
+        }
+
+        private void Start() => ChangedLocale(LocalizationSettings.SelectedLocale);
+
+        private void OnEnable() => LocalizationSettings.SelectedLocaleChanged += ChangedLocale;
+
+        private void OnDisable() => LocalizationSettings.SelectedLocaleChanged -= ChangedLocale;
+
+        private void ChangedLocale(Locale newLocale)
+        {
+            var tmpDropdownOptions = _dropdownOptions
+                                     .Select(dropdownOption =>
+                                                 new TMP_Dropdown.OptionData(
+                                                     dropdownOption.GetLocalizedString()))
+                                     .ToList();
+            _tmpDropdown.options = tmpDropdownOptions;
+        }
     }
 
-    private void Start() => ChangedLocale(LocalizationSettings.SelectedLocale);
-
-    private void OnEnable() => LocalizationSettings.SelectedLocaleChanged += ChangedLocale;
-
-    private void OnDisable() => LocalizationSettings.SelectedLocaleChanged -= ChangedLocale;
-
-    private void ChangedLocale(Locale newLocale)
+#if UNITY_EDITOR
+    public abstract class AddLocalizeDropdown
     {
-        var tmpDropdownOptions = _dropdownOptions
-                                 .Select(dropdownOption =>
-                                             new TMP_Dropdown.OptionData(
-                                                 dropdownOption.GetLocalizedString()))
-                                 .ToList();
-        _tmpDropdown.options = tmpDropdownOptions;
+        [MenuItem("CONTEXT/TMP_Dropdown/Localize", false, 1)]
+        private static void AddLocalizeComponent() =>
+            Selection.activeGameObject?.AddComponent<LocalizeDropdown>();
     }
-}
-
-public abstract class AddLocalizeDropdown
-{
-    [MenuItem("CONTEXT/TMP_Dropdown/Localize", false, 1)]
-    private static void AddLocalizeComponent() =>
-        Selection.activeGameObject?.AddComponent<LocalizeDropdown>();
+#endif
 }
