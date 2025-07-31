ea6d6cbe564a52b5a4d1047d018ee6b068ff560f
nnra6864 <enenragd@gmail.com>
2025-07-31 18:29:07 +0200

    Added ColosseumSun script to make the volumetric light a bit more dynamic

---
+++ Katana/Assets/Scripts/Core/ColosseumSun.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Core
+{
+    public class ColosseumSun : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotationSpeed = new(0, 2.5f, 0);
+        private void Update() => transform.Rotate(_rotationSpeed * Time.deltaTime);
+    }
+}
