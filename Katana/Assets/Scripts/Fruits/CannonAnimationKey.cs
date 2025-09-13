73d0e45d53c93d6653fc406ba32795037885782e
nnra6864 <enenragd@gmail.com>
2025-09-13 12:24:38 +0200

    Made the Cannon Animation Key inspector cleaner

--- Katana/Assets/Scripts/Fruits/CannonAnimationKey.cs
+++ Katana/Assets/Scripts/Fruits/CannonAnimationKey.cs
@@ -1,4 +1,5 @@
 using System;
+using ArtificeToolkit.Attributes;
 using UnityEngine;
 
 namespace Assets.Scripts.Fruits
@@ -8,13 +9,22 @@ namespace Assets.Scripts.Fruits
     {
         public Vector2 DurationRange;
         public AnimationCurve Curve;
+        [FoldoutGroup("Cannon")]
         public Vector3 CannonPositionOffsetMin;
+        [FoldoutGroup("Cannon")]
         public Vector3 CannonPositionOffsetMax;
+        [FoldoutGroup("Cannon")]
         public bool TranslateCannonPosition;
+
+        [FoldoutGroup("Barrel")]
         public Vector3 BarrelPositionOffsetMin;
+        [FoldoutGroup("Barrel")]
         public Vector3 BarrelPositionOffsetMax;
+        [FoldoutGroup("Barrel")]
         public bool TranslateBarrelPosition;
+        [FoldoutGroup("Barrel")]
         public Vector3 BarrelRotationOffsetMin;
+        [FoldoutGroup("Barrel")]
         public Vector3 BarrelRotationOffsetMax;
     }
 }
